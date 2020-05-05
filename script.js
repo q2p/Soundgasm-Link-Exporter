0834d61bd87a99be76d3d5f61369c02dc9724cc5
q2p <q2p@users.noreply.github.com>
2020-05-05 14:42:00 +0700

    Script

---
+++ script.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Soundgasm Link Extractor
+// @version      0.1
+// @include      https://soundgasm.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function gn() {
+        let el = document.getElementById("jp_audio_0");
+        if (el != undefined && el != null) {
+            let src = el.src;
+            if (src != undefined && src != null && src.length > 4) {
+                var anc = document.createElement("a");
+                anc.href = src;
+                anc.innerText = src;
+                document.body.append(anc);
+                return;
+            }
+        }
+        setTimeout(gn, 500);
+    };
+
+    gn();
+})();
