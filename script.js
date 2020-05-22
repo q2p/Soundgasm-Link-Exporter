c38894b112c23315fe1f54af7201b5c9fc9ff2fc
q2p <q2p@users.noreply.github.com>
2020-05-22 14:12:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 // ==UserScript==
-// @name         Soundgasm.net Link Exporter
+// @name         Soundgasm.net Downloader
 // @description  This is a simple script that shows you direct a link to audio file that you want to download.
 // @author       q2p
 // @namespace    q2p
-// @version      0.2
+// @version      0.3
 // @include      https://soundgasm.net/*
 // @grant        none
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
 	'use strict';
-
-	function gn() {
-		const src_el = document.getElementById("jp_audio_0");
-		const title_el = document.getElementsByClassName("jp-title");
-		if (src_el && title_el && title_el.length !== 0) {
-			const src = src_el.src;
-			const title = title_el[0].textContent;
-			if (src && src.length > 4 && title) {
-				const filename = title+".m4a";
-				const anc = document.createElement("a");
-				anc.href = src;
-				anc.download = filename;
-				anc.textContent = "Save As ("+src+")";
-				document.body.appendChild(anc);
-				return;
-			}
+	window.addEventListener("load", () => {
+		if (!document.getElementById("jquery_jplayer_1")) {
+			return;
 		}
-		setTimeout(gn, 200);
-	}
-
-	gn();
+		const interval = setInterval(() => {
+			const src_el = document.getElementById("jp_audio_0");
+			const title_el = document.getElementsByClassName("jp-title");
+			if (src_el && title_el && title_el.length !== 0) {
+				const src = src_el.src;
+				const title = title_el[0].textContent;
+				if (src && src.length > 4 && title) {
+					clearInterval(interval);
+					const link = document.createElement("a");
+					link.href = src;
+					link.download = title+".m4a";
+					link.textContent = "Download Link";
+					document.body.appendChild(link);
+				}
+			}
+		}, 200);
+	});
 })();
