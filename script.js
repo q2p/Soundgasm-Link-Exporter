de85086a8d3a33543c9076ef932e6299e6114f75
q2p <q2p@users.noreply.github.com>
2020-05-22 13:45:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // @description  This is a simple script that shows you direct a link to audio file that you want to download.
 // @author       q2p
 // @namespace    q2p
-// @version      0.1
+// @version      0.2
 // @include      https://soundgasm.net/*
 // @grant        none
 // ==/UserScript==
@@ -12,13 +12,17 @@
 	'use strict';
 
 	function gn() {
-		const el = document.getElementById("jp_audio_0");
-		if (el) {
-			const src = el.src;
-			if (src && src.length > 4) {
+		const src_el = document.getElementById("jp_audio_0");
+        const title_el = document.getElementsByClassName("jp-title");
+		if (src_el && title_el && title_el.length !== 0) {
+			const src = src_el.src;
+            const title = title_el[0].textContent;
+			if (src && src.length > 4 && title) {
+                const filename = title+".m4a";
 				const anc = document.createElement("a");
 				anc.href = src;
-				anc.textContent = src;
+                anc.download = filename;
+				anc.textContent = "Save As ("+src+")";
 				document.body.appendChild(anc);
 				return;
 			}
